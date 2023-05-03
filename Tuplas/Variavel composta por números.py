54af68a64ef407e4e819f868c142738cf6669556
Leonardo Cassauara <124696947+leonardocassauara@users.noreply.github.com>
2023-05-03 12:00:34 -0300

    Tupla, while loop. print, input, range, slicing

--- Tuplas/Variavel composta por números.py
+++ Tuplas/Variavel composta por números.py
@@ -1,5 +1,5 @@
 number = int(input('Digite um número entre 0 e 20: '))
-extenso = ('zero','um','dois','três','quatro','cinco','seis','sete','oito','nove','dez','onze','doze','treze','quatorze','quinze','dezesseis','dezessete','dezoito','dezenove','vinte')
+number_extenso = ('zero','um','dois','três','quatro','cinco','seis','sete','oito','nove','dez','onze','doze','treze','quatorze','quinze','dezesseis','dezessete','dezoito','dezenove','vinte')
 while number not in range(0, 21):
     number = int(input('Tente novamente. Digite um número entre 0 e 20: '))
-print(f'Você digitou o número {extenso[number]}')
+print(f'Você digitou o número {number_extenso[number]}')
